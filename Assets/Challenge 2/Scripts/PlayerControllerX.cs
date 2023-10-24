b6612d746afde3c86405dab525e2182d937c7812
Jaimin Parmar <jaimins100@gmail.com>
2023-10-24 19:53:05 +0530

    delay dog spawning

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    bool isDogReady = true;
+    float waitTime = 0.5f;
 
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isDogReady)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            isDogReady = false;
+            Invoke(nameof(DogReady), waitTime);
         }
+    }
+
+    void DogReady()
+    {
+        isDogReady = true;
     }
 }
