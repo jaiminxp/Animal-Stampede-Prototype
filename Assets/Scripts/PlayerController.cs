5122dc5f05dbf2024814e4cc6458f339bee7639a
Jaimin Parmar <jaimins100@gmail.com>
2023-10-25 16:08:51 +0530

    Vertical player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,10 @@ public class PlayerController : MonoBehaviour
 
     public GameObject projectile;
 
+    float zMin = 0.0f;
+    float zMax = 10.0f;
+    float verticalInput;
+
     void Update()
     {
         if (transform.position.x < -xRange)
@@ -22,8 +26,22 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
 
+        if (transform.position.z < zMin)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y,
+            zMin);
+        }
+        if (transform.position.z > zMax)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y,
+            zMax);
+        }
+
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
